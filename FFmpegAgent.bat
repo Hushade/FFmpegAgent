149259d56c7b0d7a081c54e20cacd979ffb93148
Hushade <uki.110m.saito@gmail.com>
2025-11-25 17:16:28 +0900

    SJIS 一回の起動で一度の変換に限定 処理の簡素化

--- FFmpegAgent.bat
+++ FFmpegAgent.bat
@@ -1,8 +1,7 @@
 @echo off
-setlocal enableDelayedExpansion
-cd %~dp0
+setlocal enabledelayedexpansion
 
-:LOOP
+cd %~dp0
 
 set inifile=%~dp0Config.ini
 
@@ -10,19 +9,21 @@ rem iniファイルを変数ファイルとして簡単に読み出し ｢;｣から始まるコメント行を除外
 for /f "tokens=1,2 delims==" %%a in ('type %inifile% ^| findstr /v "^;"') do (set "%%a=%%b")
 
 set /p "input=入力ファイルのパス> "
-set /p "start=元動画の切り取りの開始時間> "
-set /p "end=切り取られた動画の終了時間> "
+rem 引用符を除去
+set input=%input:"=%
 
 for %%A in ("%input%") do (
-    set "input_name=%%~nA"
-    set "input_dir=%%~dpA"
+    set dirname=%%~dpA
+    set filename=%%~nxA
 )
-rem 変数に代入（元のディレクトリを保持）
-set "output=%input_dir%%input_name%-Clip.mp4"
+for %%B in ("%filename%") do (
+    set base=%%~nB
+    set ext=%%~xB
+)
+set output=!dirname!!base!-Clip!ext!
 
-ffmpeg -ss %start% -to %end% -i %input% -c:v %video_encoder% -pix_fmt %color_fmt% -color_range %color_range% -aspect:v %aspect% -r:v %framerate% %video_quality% -preset %preset% -c:a %audio_encoder% %audio_quality% %option% %output%
+set /p "start=動画の切り取りの開始時間(MM:SS)> "
+set /p "end=動画の切り取りの終了時間(MM:SS)> "
 
-pause
-GOTO LOOP
-
+ffmpeg -ss %start% -to %end% -i "%input%" -c:v %video_encoder% -pix_fmt %color_fmt% -color_range %color_range% -aspect:v %aspect% -r:v %framerate% %video_options% -c:a %audio_encoder% %audio_options% %option% "%output%"
 endlocal
